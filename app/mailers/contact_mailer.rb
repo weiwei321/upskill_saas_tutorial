91c1b545423f1cdd170a9cd81ac16b86f5eb7a70
Wei Li <weili60616@gmail.com>
2017-01-22 02:37:01 +0000

    fixed error

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-  default to 'weili60616@gmail.com'
+  default to: 'weili60616@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
+    
     mail(from: email, subject: 'Contact Form Message')
-  
   end
 end
