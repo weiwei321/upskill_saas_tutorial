5918e207ed66dce779ceacef837ddc709c16f64b
Wei Li <weili60616@gmail.com>
2017-01-26 23:51:05 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,7 +19,12 @@ class ProfilesController < ApplicationController
     end
   end
   
-  
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find (params[:user_id] )
+    @profile = @user.profile
+    
+  end
   
   
   
