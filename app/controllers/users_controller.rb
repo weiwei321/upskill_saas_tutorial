90c76ff813ac246cb9f23af088f0928a4ed89cb3
Wei Li <weili60616@gmail.com>
2017-01-27 18:56:33 +0000

    Added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  def index
+    
+  end 
+  
   # GET /users/:id
   def show
     @user = User.find( params[:id] )
   end
+  
   
   
 end
